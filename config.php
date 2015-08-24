2b5c1c77e3daee740ec4c1e7d0c38befee0e1ebe
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-24 10:30:55 +0800

    setting error and GD in config.php

--- config.php
+++ config.php
@@ -6,11 +6,22 @@
 // このファイル(config.php)までのフルパス（のディレクトリ名）を取ってきなさいというのがdirnameです
 // →→→ config.phpが入っている「picture_board」の中にあるimages(thumbnails)もしくはthumnailsをくっつけたものがIMAGES_DIR（THUMBNAIL_DIR）にしなさいという定数
 define('IMAGES_DIR', dirname($_SERVER['SCRIPT_FILENAME']).'/images');
-define('THUMBNAILS_DIR', dirname($_SERVER['SCRIPT_FILENAME']).'/thumnails');
+define('THUMBNAIL_DIR', dirname($_SERVER['SCRIPT_FILENAME']).'/thumnails');
 
 // 何を指定しているかを一旦確認している→実際フルパスで指定している
-var_dump(IMAGES_DIR);
-var_dump(THUMBNAILS_DIR);
+// var_dump(IMAGES_DIR);
+// var_dump(THUMBNAILS_DIR);
+
+define('THUMBNAIL_WIDTH', 72);
+define('MAX_FILE_SIZE', 307200); // 300kb = 1kb/1024bytes * 300
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+// GD
+if (!function_exists('imagecreatetruecolor')) {
+	echo "GDがインストールされていません!";
+	exit;
+}
 
 
 // define('DSN', 'mysql:host=localhost;dbname=contact_dotinstall');
@@ -20,6 +31,5 @@ var_dump(THUMBNAILS_DIR);
 // define('SITE_URL', 'http://192.168.33.10/php/contact/');
 // define('ADMIN_URL', SITE_URL.'admin/');
 
-// error_reporting(E_ALL & ~E_NOTICE);
 
 // session_set_cookie_params(0, '/contact/');
